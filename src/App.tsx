df885cd955a5ad83cd9dfa7f56a2d6f2d89e0721
ericcarmi <ecarmi@protonmail.com>
2022-12-06 20:39:50 -0800

    rows

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,30 @@ import {Hexagons} from './Hexagon';
 const hexsize = 100;
 const colors = ['rgba(150,0,0,0.9)','rgba(0,150,0,0.9)']
 
-/*
-useState hooks are needed to update through style={{}} in html return without 200+ render warning
-so the selectors aren't used in the styled button, which is kind of annoying
-maybe they just need to be two different things, don't control the color with hooks, but keep the hooks anyway
-2 representations for the same thing, is there a way to guarantee they are in sync?
-
-but what selectors will set the hovered ones to active when the mouse is down?
-
-problem with useState is because onMouseEnter is changing all of them
-just make an array, they all have different colors
----
-
-almost working but it only updates after mouseup, even without using mouseup/down logic
-might need to put the updateColor functionality inside useEffect with a callback?
-wtf is the point of the hooks then if you have to use a separate callback, they are supposed to update as is
----
-moved to its own component still nothing
-is it because the style doesn't get updated? from the log, it's clear that the state variable is updated
-passing to styled.div works though
-
-
-*/
 
 
 
 function App() {
   const [isMouseDown, setMouseDown] = useState(false);
-  
-
+  const [numRows, setNumRows] = useState(20);
+  const [numCols, setNumCols] = useState(20);
 
   return (
     <div className="App" onMouseDown={() => setMouseDown(true)} onMouseUp={() => setMouseDown(false)}>
       <header className="App-header">
-    <Hexagons isMouseDown={isMouseDown}/>
-      <RulesInput/>
+    <Hexagons isMouseDown={isMouseDown} numrows={numRows}/>
+      <RulesInput 
+      />
     <Rules> rules </Rules>
-      <RowsInput/>
+      <RowsInput
+          onChange={(e) => e.target.value}
+          onKeyDown={(e) => {
+            if(e.code === 'Enter' && !isNaN(Number(e.currentTarget.value))){
+              // console.log(Number(e.currentTarget.value));
+              setNumRows(Number(e.currentTarget.value));
+            }
+          }}
+          />
     <Rows> rows </Rows>
       <ColsInput/>
     <Cols> cols </Cols>
--- src/Hexagon.tsx
+++ src/Hexagon.tsx
@@ -7,12 +7,13 @@ const colors = ['rgba(150,0,0,0.9)','rgba(0,150,0,0.9)']
 
 interface IHexagons {
   isMouseDown? : boolean,
+  numrows: number;
 }
 
   
-export const Hexagons = ({isMouseDown}:IHexagons) => {
+export const Hexagons = ({isMouseDown, numrows}:IHexagons) => {
   
-  const numrows = 20;
+  // const numrows = 20;
   const numcols = 20;
   const numhex = numcols * numrows;
 
@@ -22,14 +23,40 @@ export const Hexagons = ({isMouseDown}:IHexagons) => {
   const updateColor = (id:number, newcolor:string) => {
     const r = backgroundColor;
     r[id] = newcolor;
-    setBackgroundColor({...r});
+    setBackgroundColor({...r}); // need to use ({...r})
     const q = isActive;
-      q[id] = newcolor == 'white' ? true : false;
-      setActive({...q});
+    q[id] = newcolor == 'white' ? true : false;
+    setActive({...q});
+    
   }
   
+  const resetAll = () => {
+    const r = Array(numhex).fill('black');
+    setBackgroundColor({...r});
+    const q = Array(numhex).fill(false);
+    setActive({...q});
+  }
   
+
+  /*
+  instead of using hexagon divs as inputs, use the keys?
+  check if not negative
+  with it not rotated anymore, down is +2, up is -2
+  up-left is ... columns looks repeated/upsampled, 0, 0, 1, 1, ...
+  maybe just give each a unique id
+
+  up-left is -21 with 20 rows, except for second row which is -20
+  dn-left is +20 or +19
+  dn-right is +21 or +20
+  up-right is -20 or -19
   
+  traversing a column will return a constant for the last digit: 19, 59, 99, 139...just a constant offset
+   
+  */
+
+  const checkNeighbors = () => {
+  
+  }
   
 
   return(
@@ -37,7 +64,8 @@ export const Hexagons = ({isMouseDown}:IHexagons) => {
         {[...Array(numrows)].map((x, i) =>
           [...Array(numcols)].map((y, j) => 
           <Hexagon key={i*numcols+j} 
-            style={{background: backgroundColor[i*numcols+j], left:100+i*50 - 25 * (j%2), top: 37*(j+1)}}
+            isactive={isActive[i*numcols+j]}
+            style={{background: backgroundColor[i*numcols+j], left:50+i*72 + 36 * (j%2), top:200 + 25*(j+1)}}
             onMouseDown={(e) => {if(e.shiftKey){
                       updateColor(i*numcols+j, 'black');
                     }
@@ -57,8 +85,12 @@ export const Hexagons = ({isMouseDown}:IHexagons) => {
                   }
             }}
 
-          />
+          >
+            {j*numrows+i}
+          </Hexagon>
+  
     ))}
+    <ResetButton onClick={resetAll}/>
     </Grid>
   );
 }
@@ -69,28 +101,54 @@ const Hexagon = styled.div.attrs((props : {top : number, left : number, backgrou
   position: absolute;
   width: ${hexsize}px;
   background: ${props => props.background};
-  height: 50px;
+  height: ${hexsize}px;
+  font-size:16px;
+  border: none;
+  outline: none;
+  color: ${props => props.isactive ? 'black' : 'white'};
+  top: calc(${props => props.top} * 1px);
+  left: calc(${props => props.left} * 1px);
+  clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
+  transform: rotateZ(0deg);
+  user-select: none;
+`
+
+const HexagonBorder = styled.div.attrs((props : {top : number, left : number, background : string, mousedown : boolean, isactive : boolean}) => props)`
+  position: absolute;
+  width: 105%
+  background: ${props => props.background};
+  height: 105%;
   border: none;
   outline: none;
   top: calc(${props => props.top} * 1px);
   left: calc(${props => props.left} * 1px);
   clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
-  transform: rotateZ(90deg);
-
-  &:hover{
-    background: ${props => props.mousedown ? 'white' : 'red'};
-  }
-
-  &:active{
-    
-    background: ${props => props.isactive ? 'white' : 'black'};
-  }
-    
+  transform: rotateZ(0deg);    
 `
 
 const Grid = styled.div`
   position: absolute;
   background: gray;
-  width: 1500px;
-  height: 900px;
+  width: 100%;
+  height: 100%;
+`
+
+const ResetButton = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background: rgb(150,0,0);
+  transition: background 0.1s;
+  position: absolute;
+  left: 80%;
+  top: 1%;
+  cursor: pointer;
+  &:hover{
+    background: rgb(225,0,0);
+  }  
+  &:active{
+    background: rgb(255,0,0);
+  }  
+
+
 `
