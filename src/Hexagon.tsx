bf2d15f5ae49fa0cdfa0162c37e03659a438c4ec
ericcarmi <ecarmi@protonmail.com>
2022-12-07 08:37:03 -0800

    neighbors almost done

--- src/Hexagon.tsx
+++ src/Hexagon.tsx
@@ -19,6 +19,7 @@ export const Hexagons = ({isMouseDown, numrows}:IHexagons) => {
 
   const [backgroundColor, setBackgroundColor] = useState<string[]>(Array(numhex).fill('black'));
   const [isActive, setActive] = useState<boolean[]>(Array(numhex).fill(false));
+  const [activeCells, setActiveCells] = useState([]);
 
   const updateColor = (id:number, newcolor:string) => {
     const r = backgroundColor;
@@ -51,12 +52,53 @@ export const Hexagons = ({isMouseDown, numrows}:IHexagons) => {
   up-right is -20 or -19
   
   traversing a column will return a constant for the last digit: 19, 59, 99, 139...just a constant offset
-   
+  ---
+  it isn't first row/col...it alternates between +20 and +21 when going dn-right
+  switched back to numbering the same way colors are done, i*numcols + j
+  dn-right is +1 if even, +21 if odd
+  dn-left is +1 if odd, -19 if even
+  
   */
 
-  const checkNeighbors = () => {
-  
+  const checkNeighbors = (row: number, col: number) => {
+    // use i for rows, j for columns?  no it's the other way around
+    // up is always -2, down is always +2
+    const id = col*numcols + row // the unique id/key
+    const shifts = [2,-2]
+    
+    if( row % 2 === 0 ) { // even
+      // u-r : -1
+      // u-l : -21
+      // d-l : -19
+      // d-r : +1
+      shifts.push(-1, -21, -19, 1)
+    }
+    else { // odd
+      // u-r : +19
+      // u-l : -1
+      // d-l : +1
+      // d-r : +21
+      shifts.push(19, -1, 1, 21)
+    }
+
+    const neighborsToCheck = shifts.map((i) => i + id)
+    console.log(neighborsToCheck);
+    neighborsToCheck.map((i) => {
+      if(i >= 0) {
+        console.log(isActive[i]);
+        
+      }
+    })
+    
+    
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // checkNeighbors;
+    },100);
+    return () => clearInterval(interval);
+  },[])
   
 
   return(
@@ -65,13 +107,16 @@ export const Hexagons = ({isMouseDown, numrows}:IHexagons) => {
           [...Array(numcols)].map((y, j) => 
           <Hexagon key={i*numcols+j} 
             isactive={isActive[i*numcols+j]}
-            style={{background: backgroundColor[i*numcols+j], left:50+i*72 + 36 * (j%2), top:200 + 25*(j+1)}}
-            onMouseDown={(e) => {if(e.shiftKey){
+            background={backgroundColor[i*numcols+j]}
+            style={{left:650+i*72 + 36 * (j%2), top:200 + 25*(j+1)}}
+            onMouseDown={(e) => {
+                  if(e.shiftKey){
                       updateColor(i*numcols+j, 'black');
                     }
                     else {
                       updateColor(i*numcols+j, 'white');
                     }
+              checkNeighbors(j,i)
                   } }
             onMouseEnter={(e) => { 
                   if(isMouseDown){
@@ -86,12 +131,12 @@ export const Hexagons = ({isMouseDown, numrows}:IHexagons) => {
             }}
 
           >
-            {j*numrows+i}
+            {i*numcols+j}<br/>{j % 2 === 0 ? 'e' : 'o'}
           </Hexagon>
   
-    ))}
-    <ResetButton onClick={resetAll}/>
-    </Grid>
+      ))}
+      <ResetButton onClick={resetAll}/>
+      </Grid>
   );
 }
 
@@ -111,6 +156,10 @@ const Hexagon = styled.div.attrs((props : {top : number, left : number, backgrou
   clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
   transform: rotateZ(0deg);
   user-select: none;
+  justify-content: center;
+  align-content: center;
+  vertical-align: middle;
+  // line-height: 50px;
 `
 
 const HexagonBorder = styled.div.attrs((props : {top : number, left : number, background : string, mousedown : boolean, isactive : boolean}) => props)`
@@ -140,7 +189,7 @@ const ResetButton = styled.div`
   background: rgb(150,0,0);
   transition: background 0.1s;
   position: absolute;
-  left: 80%;
+  left: 70%;
   top: 1%;
   cursor: pointer;
   &:hover{
