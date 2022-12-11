ab5ec68f830aeaf3704f9e96d8c01bd50fe7cd41
ericcarmi <ecarmi@protonmail.com>
2022-12-11 14:10:32 -0800

    better refactor?

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,219 @@ import styled from 'styled-components';
 import './App.css';
 import {useState, useEffect} from 'react';
 import {Hexagons} from './Hexagon';
-
+import {rules} from './rules';
 
 
 function App() {
   const [isMouseDown, setMouseDown] = useState(false);
-  const [numRows, setNumRows] = useState(30);
-  const [numCols, setNumCols] = useState(50);
-  const hexsize = 2**5;
-  const updateInterval = 300;
+  const [numRows, setNumRows] = useState(80);
+  const [numCols, setNumCols] = useState(100);
+  const hexsize = 2**4;
+  const updateInterval = 1000;
+
+
+  // const numrows = 20;
+  // const numcols = 20;
+  const [numhex, setNumHex]  = useState(numCols * numRows);
+
+  const [backgroundColor, setBackgroundColor] = useState<string[]>(Array(numhex).fill('black'));
+  const [isActive, setActive] = useState<boolean[]>(Array(numhex).fill(false));
+  const [allCells, setAllCells] = useState([...Array(numhex)].map((_,i) => i));
+  const [boundaryCells, setBoundaryCells] = useState(Array(0));
+  const [interiorCells, setInteriorCells] = useState(Array(0));
+  const [shouldIterate, setShouldIterate] = useState(false);
+  const [shouldInit, setShouldInit] = useState(true);
+
+  // rename to updateCell
+  const updateColor = (id:number, newcolor:string) => {
+    const r = backgroundColor;
+    r[id] = newcolor;
+    setBackgroundColor([...r]); 
+    const q = isActive;
+    q[id] = r[id] === 'white' ? true : false;
+    setActive([...q]);
+    // setActive(prev => [...prev.filter(i => i === q[id])]); // not workin, could be faster? idk
+   
+  }
+  const updateAll = (active: Array<number>, dead: Array<number>) => {
+    const q = isActive;
+    const r = backgroundColor;
+    active.map(i => {
+      if(i > 0)
+        q[i] = true;
+        r[i] = 'white';
+    });
+    dead.map(i => {
+      if(i > 0)
+        q[i] = false;
+        r[i] = 'black';
+    });
+    setBackgroundColor([...r]); 
+    setActive([...q]);
+   
+  }
+
+  const updateBoundary = (shift: boolean) => {
+    const q = isActive;
+    const r = backgroundColor;
+
+    if(shift) {
+      if(q[0] === false){
+        boundaryCells.map(i => {
+            q[i] = true;
+            r[i] = 'white';
+        })
+      }
+      else{
+        boundaryCells.map(i => {
+            q[i] = false;
+            r[i] = 'black';
+        })
+      
+      }
+    }
+    else{
+        interiorCells.map(i => {
+            q[i] = true;
+            r[i] = 'white';
+        })
+    }
+    setBackgroundColor([...r]); 
+    setActive([...q]);
+
+    
+  };
+  
+  const resetAll = () => {
+    const r = Array(numhex).fill('black');
+    setBackgroundColor([...r]);
+    const q = Array(numhex).fill(false);
+    setActive([...q]);
+  }
+
+
+  function initCells() {
+      setNumHex(numCols*numRows);
+      setAllCells([...Array(numCols*numRows)].map((_,i) => i));
+      // console.log(numrows,numcols,numhex);
+  
+      let b = [];
+      for(let i = 0; i < numRows; i++){
+        b.push(i * numRows);
+        b.push(numRows - 2 + i * numRows+1);
+      }
+      for(let i = 0; i < Math.round(numCols/2); i++){
+        b.push(i * 2);
+        b.push(i * 2 + 1);
+        b.push(i * 2 + numRows * numCols - numRows + 1);
+        b.push(i * 2 + numRows * numCols - numRows);
+      }
+
+      setBoundaryCells(b);
+      // console.log(boundaryCells, interiorCells);
+    
+
+      setInteriorCells(allCells.filter(i => !boundaryCells.includes(i)));
+      // console.log(boundaryCells, interiorCells);
+
+  
+  };
+
+  useEffect(() => {
+    
+    if(shouldInit){
+      initCells();
+      setShouldInit(false);
+    }
+  },[setShouldInit, shouldInit])
+
+
+  const update = () => {
+      let nextActiveCells: Array<number> = [];
+      let nextDeadCells: Array<number> = [];
+      interiorCells.map((i) => {
+        // const shifts = [-2] // format: up, ur, dr, dn, dl, dr, needs to communicate properly with format of rule
+        // is this fixed with recognizing it as a symmetry? many formats are possible
+        // can format be learned with a different program? some use of logic and statistics (if data can be got)
+        // now doing the correct format of regular hexagon...
+      const shifts = [-1]
+      const x = Math.floor(i/2)%numRows;
+        if(x < (numRows/2)) { // even (0)
+          // it always uses numrows even when numrows != numcols
+          // shifts.push(-1, 1, 2, -numrows+1, -numrows-1);
+          shifts.push(numRows-1,numRows, 1, -numRows, -numRows-1);
+        }
+        else { // odd (1) ...it's different now with regular tiling, this is the condition for second half of the rows
+          // shifts.push(numRows-1, numRows+1, 2, 1, -1);
+          shifts.push(numRows,numRows+1, 1, -numRows+1, -numRows);
+        }
+        // console.log(shifts.map((k) => i + k));
+      // console.log(i,Math.floor(i/2)%numRows);
+
+        let neighbors = isActive[i] ? '1' : '0';
+
+        shifts.map((s,index) => {
+          const a = s + i;
+          if(a>0) {
+          if(isActive[a]){
+            neighbors += '1';
+          }
+          else{
+            neighbors += '0';
+          }
+        }
+        })
+          const stayAlive = rules[neighbors as keyof typeof rules] === '1' ? true : false
+          if(stayAlive){
+            nextActiveCells.push(i);
+          }
+          else{
+            nextDeadCells.push(i);
+          }
+
+      })
+
+      // rule to always set boundary to dead? should call a function on both, or fxn pointer since there will be multiple
+    //   boundaryCells.map((i) => {
+    //   nextDeadCells.push(i);
+    // })
+    // still able to propagate away from boundary or reflect
+
+      updateAll(nextActiveCells,nextDeadCells);
+
+
+      // console.log(activeCells);
+    // console.log('completed update function')
+  }        
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     let x = numcols*numRows/2 + numRows - 2;
+  //     updateColor(x, 'white');
+  //   },updateInterval);
+  //   return () => clearInterval(interval);
+  // },[updateColor,])
+
+  // or you don't need to pass row,col? just figure it out by the number in activeCell? just even or odd
+  useEffect(() => {
+    if(shouldIterate){
+    const interval = setInterval(() => {
+      update();
+        
+    },updateInterval);
+    return () => clearInterval(interval);
+    }
+  },[shouldIterate, resetAll])
+
+
 
   return (
     <div className="App" onMouseDown={() => setMouseDown(true)} onMouseUp={() => setMouseDown(false)}>
-      <header className="App-header">
-    <Hexagons hexsize={hexsize} updateInterval={updateInterval} isMouseDown={isMouseDown} numrows={numRows} numcols={numCols}/>
+    <Hexagons hexsize={hexsize} isMouseDown={isMouseDown} numRows={numRows} numCols={numCols}
+        isActive={isActive}
+        backgroundColor={backgroundColor}
+        updateColor={updateColor}
+        />
       <RowsInput
           onChange={(e) => e.target.value}
           onKeyDown={(e) => {
@@ -38,7 +237,11 @@ function App() {
     <Cols> cols </Cols>
       <IntervalInput/>
     <Interval onChange = {event => console.log(event.target)}> interval </Interval>
-     </header>
+
+      <ResetButton onClick={resetAll}/>
+      <StartButton isIterating={shouldIterate} onClick={() => setShouldIterate(!shouldIterate)}/>
+      <NextButton onClick={() => update()} isIterating={shouldIterate} />
+      <BoundaryButton onClick={(e) => updateBoundary(e.shiftKey ? false : true)} isIterating={shouldIterate} />
 
     </div>
   );
@@ -103,5 +306,88 @@ const Interval = styled.div`
   color: white;
 `
 
+
+const ResetButton = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background: rgb(150,0,0);
+  transition: background 0.1s;
+  position: absolute;
+  left: 70%;
+  top: 1%;
+  cursor: pointer;
+  &:hover{
+    background: rgb(225,0,0);
+  }  
+  &:active{
+    background: rgb(255,0,0);
+  }  
+
+
+`
+const StartButton = styled.div<{isIterating: boolean}>`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background: ${p => p.isIterating ? 'rgb(0,150,0)' : 'rgb(150,50,0)'};
+  transition: background 0.1s;
+  position: absolute;
+  left: 60%;
+  top: 1%;
+  cursor: pointer;
+
+  &:hover{
+    background: ${p => p.isIterating ? 'rgb(0,180,0)' : 'rgb(180,80,0)'};
+  }  
+
+  &:active{
+    background: ${p => p.isIterating ? 'rgb(0,220,0)' : 'rgb(220,110,0)'};
+  }  
+
+
+`
+const NextButton = styled.div<{isIterating: boolean}>`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background: ${p => p.isIterating ? 'rgb(150,0,150)' : 'rgb(150,0,150)'};
+  transition: background 0.1s;
+  position: absolute;
+  left: 65%;
+  top: 1%;
+  cursor: pointer;
+
+  &:hover{
+    background: ${p => p.isIterating ? 'rgb(190,0,190)' : 'rgb(190,0,190)'};
+  }  
+
+  &:active{
+    background: ${p => p.isIterating ? 'rgb(220,0,220)' : 'rgb(220,0,220)'};
+  }  
+
+
+`
+const BoundaryButton = styled.div<{isIterating: boolean}>`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background: ${p => p.isIterating ? 'rgb(0,0,150)' : 'rgb(0,0,150)'};
+  transition: background 0.1s;
+  position: absolute;
+  left: 75%;
+  top: 1%;
+  cursor: pointer;
+
+  &:hover{
+    background: ${p => p.isIterating ? 'rgb(0,0,180)' : 'rgb(0,0,180)'};
+  }  
+
+  &:active{
+    background: ${p => p.isIterating ? 'rgb(0,0,220)' : 'rgb(0,0,220)'};
+  }  
+
+
+`
 
 export default App;
--- src/Hexagon.tsx
+++ src/Hexagon.tsx
@@ -51,34 +51,15 @@ the conditions should be for the next set of rules, rules(i+1) <- rules(i)
 /*
  0010000 -> dl when 653
 */
-const rules = 
-{
-"0000000" : "0" ,"0000001" : "1" ,"0000010"  : "1" ,"0000011" : "0" ,"0000100" : "1" ,"0000101" : "0" ,"0000110" : "0" ,"0000111" : "0" ,
-"0001000" : "1" ,"0001001" : "0" ,"0001010"  : "0" ,"0001011" : "0" ,"0001100" : "0" ,"0001101" : "0" ,"0001110" : "0" ,"0001111" : "0" ,
-"0010000" : "1" ,"0010001" : "0" ,"0010010"  : "0" ,"0010011" : "0" ,"0010100" : "0" ,"0010101" : "0" ,"0010110" : "0" ,"0010111" : "0" ,
-"0011000" : "0" ,"0011001" : "0" ,"0011010"  : "0" ,"0011011" : "0" ,"0011100" : "0" ,"0011101" : "0" ,"0011110" : "0" ,"0011111" : "0" ,
-"0100000" : "1" ,"0100001" : "0" ,"0100010"  : "0" ,"0100011" : "0" ,"0100100" : "0" ,"0100101" : "0" ,"0100110" : "0" ,"0100111" : "0" ,
-"0101000" : "0" ,"0101001" : "0" ,"0101010"  : "0" ,"0101011" : "0" ,"0101100" : "0" ,"0101101" : "0" ,"0101110" : "0" ,"0101111" : "0" ,
-"0110000" : "0" ,"0110001" : "0" ,"0110010"  : "0" ,"0110011" : "0" ,"0110100" : "0" ,"0110101" : "0" ,"0110110" : "0" ,"0110111" : "0" ,
-"0111000" : "0" ,"0111001" : "0" ,"0111010"  : "0" ,"0111011" : "0" ,"0111100" : "0" ,"0111101" : "0" ,"0111110" : "0" ,"0111111" : "0" ,
-"1000000" : "1" ,"1000001" : "0" ,"1000010"  : "0" ,"1000011" : "0" ,"1000100" : "0" ,"1000101" : "0" ,"1000110" : "0" ,"1000111" : "0" ,
-"1001000" : "0" ,"1001001" : "0" ,"1001010"  : "0" ,"1001011" : "0" ,"1001100" : "0" ,"1001101" : "0" ,"1001110" : "0" ,"1001111" : "0" ,
-"1010000" : "0" ,"1010001" : "0" ,"1010010"  : "0" ,"1010011" : "0" ,"1010100" : "0" ,"1010101" : "0" ,"1010110" : "0" ,"1010111" : "0" ,
-"1011000" : "0" ,"1011001" : "0" ,"1011010"  : "0" ,"1011011" : "0" ,"1011100" : "0" ,"1011101" : "0" ,"1011110" : "0" ,"1011111" : "0" ,
-"1100000" : "0" ,"1100001" : "0" ,"1100010"  : "0" ,"1100011" : "0" ,"1100100" : "0" ,"1100101" : "0" ,"1100110" : "0" ,"1100111" : "0" ,
-"1101000" : "0" ,"1101001" : "0" ,"1101010"  : "0" ,"1101011" : "0" ,"1101100" : "0" ,"1101101" : "0" ,"1101110" : "0" ,"1101111" : "0" ,
-"1110000" : "0" ,"1110001" : "0" ,"1110010"  : "0" ,"1110011" : "0" ,"1110100" : "0" ,"1110101" : "0" ,"1110110" : "0" ,"1110111" : "0" ,
-"1111000" : "0" ,"1111001" : "0" ,"1111010"  : "0" ,"1111011" : "0" ,"1111100" : "0" ,"1111101" : "0" ,"1111110" : "0" ,"1111111" : "0" ,
-}
-
-
 
 interface IHexagons {
   isMouseDown? : boolean,
-  numrows: number;
-  numcols: number;
+  numRows: number;
+  numCols: number;
   hexsize: number;
-  updateInterval: number;
+  backgroundColor: Array<string>; 
+  isActive: Array<boolean>;
+  updateColor: (n: number, s: string) => void;
 }
 
 
@@ -86,244 +67,51 @@ interface IHexagons {
 
 
 
-export const Hexagons = ({isMouseDown, numrows, numcols, updateInterval, hexsize}:IHexagons) => {
+export const Hexagons = ({
+  isMouseDown, numRows, numCols, updateColor, hexsize,  backgroundColor, isActive,
+
+  }:IHexagons) => {
   
-  // const numrows = 20;
-  // const numcols = 20;
-  const [numhex, setNumHex]  = useState(numcols * numrows);
-
-  const [backgroundColor, setBackgroundColor] = useState<string[]>(Array(numhex).fill('black'));
-  const [isActive, setActive] = useState<boolean[]>(Array(numhex).fill(false));
-  const [allCells, setAllCells] = useState([...Array(numhex)].map((_,i) => i));
-  const [boundaryCells, setBoundaryCells] = useState(Array(0));
-  const [interiorCells, setInteriorCells] = useState(Array(0));
-  const [shouldIterate, setShouldIterate] = useState(false);
-  const [shouldInit, setShouldInit] = useState(true);
-
-  // rename to updateCell
-  const updateColor = (id:number, newcolor:string) => {
-    const r = backgroundColor;
-    r[id] = newcolor;
-    setBackgroundColor([...r]); 
-    const q = isActive;
-    q[id] = r[id] === 'white' ? true : false;
-    setActive([...q]);
-    // setActive(prev => [...prev.filter(i => i === q[id])]); // not workin, could be faster? idk
-   
-  }
-  const updateAll = (active: Array<number>, dead: Array<number>) => {
-    const q = isActive;
-    const r = backgroundColor;
-    active.map(i => {
-      if(i > 0)
-        q[i] = true;
-        r[i] = 'white';
-    });
-    dead.map(i => {
-      if(i > 0)
-        q[i] = false;
-        r[i] = 'black';
-    });
-    setBackgroundColor([...r]); 
-    setActive([...q]);
-   
-  }
-
-  const updateBoundary = (shift: boolean) => {
-    const q = isActive;
-    const r = backgroundColor;
-
-    if(shift) {
-      if(q[0] === false){
-        boundaryCells.map(i => {
-            q[i] = true;
-            r[i] = 'white';
-        })
-      }
-      else{
-        boundaryCells.map(i => {
-            q[i] = false;
-            r[i] = 'black';
-        })
-      
-      }
-    }
-    else{
-        interiorCells.map(i => {
-            q[i] = true;
-            r[i] = 'white';
-        })
-    }
-    setBackgroundColor([...r]); 
-    setActive([...q]);
-
-    
-  };
-  
-  const resetAll = () => {
-    const r = Array(numhex).fill('black');
-    setBackgroundColor([...r]);
-    const q = Array(numhex).fill(false);
-    setActive([...q]);
-  }
-
-
-  function initCells() {
-      setNumHex(numcols*numrows);
-      setAllCells([...Array(numcols*numrows)].map((_,i) => i));
-      // console.log(numrows,numcols,numhex);
-  
-      let b = [];
-      for(let i = 0; i < numrows; i++){
-        b.push(i * numrows);
-        b.push(numrows - 2 + i * numrows+1);
-      }
-      for(let i = 0; i < Math.round(numcols/2); i++){
-        b.push(i * 2);
-        b.push(i * 2 + 1);
-        b.push(i * 2 + numrows * numcols - numrows + 1);
-        b.push(i * 2 + numrows * numcols - numrows);
-      }
-
-      setBoundaryCells(b);
-      // console.log(boundaryCells, interiorCells);
-    
-
-      setInteriorCells(allCells.filter(i => !boundaryCells.includes(i)));
-      // console.log(boundaryCells, interiorCells);
-
-  
-  };
-
-  useEffect(() => {
-    
-    if(shouldInit){
-      initCells();
-      setShouldInit(false);
-    }
-  },[setShouldInit, shouldInit])
-
-
-  const update = () => {
-      let nextActiveCells: Array<number> = [];
-      let nextDeadCells: Array<number> = [];
-      interiorCells.map((i) => {
-        // const shifts = [-2] // format: up, ur, dr, dn, dl, dr, needs to communicate properly with format of rule
-        // is this fixed with recognizing it as a symmetry? many formats are possible
-        // can format be learned with a different program? some use of logic and statistics (if data can be got)
-        // now doing the correct format of regular hexagon...
-      const shifts = [-1]
-      const x = Math.floor(i/2)%numrows;
-        if(x < (numrows/2)) { // even (0)
-          // it always uses numrows even when numrows != numcols
-          // shifts.push(-1, 1, 2, -numrows+1, -numrows-1);
-          shifts.push(numrows-1,numrows, 1, -numrows, -numrows-1);
-        }
-        else { // odd (1) ...it's different now with regular tiling, this is the condition for second half of the rows
-          // shifts.push(numrows-1, numrows+1, 2, 1, -1);
-          shifts.push(numrows,numrows+1, 1, -numrows+1, -numrows);
-        }
-        // console.log(shifts.map((k) => i + k));
-      // console.log(i,Math.floor(i/2)%numrows);
-
-        let neighbors = isActive[i] ? '1' : '0';
-
-        shifts.map((s,index) => {
-          const a = s + i;
-          if(a>0) {
-          if(isActive[a]){
-            neighbors += '1';
-          }
-          else{
-            neighbors += '0';
-          }
-        }
-        })
-          const stayAlive = rules[neighbors as keyof typeof rules] === '1' ? true : false
-          if(stayAlive){
-            nextActiveCells.push(i);
-          }
-          else{
-            nextDeadCells.push(i);
-          }
-
-      })
-
-      // rule to always set boundary to dead? should call a function on both, or fxn pointer since there will be multiple
-    //   boundaryCells.map((i) => {
-    //   nextDeadCells.push(i);
-    // })
-    // still able to propagate away from boundary or reflect
-
-      updateAll(nextActiveCells,nextDeadCells);
-
-
-      // console.log(activeCells);
-    // console.log('completed update function')
-  }        
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     let x = numcols*numrows/2 + numrows - 2;
-  //     updateColor(x, 'white');
-  //   },updateInterval);
-  //   return () => clearInterval(interval);
-  // },[updateColor,])
-
-  // or you don't need to pass row,col? just figure it out by the number in activeCell? just even or odd
-  useEffect(() => {
-    if(shouldIterate){
-    const interval = setInterval(() => {
-      update();
-        
-    },updateInterval);
-    return () => clearInterval(interval);
-    }
-  },[shouldIterate, resetAll])
   
 
   // when setting left position, use ceil() to create tiny border around hexagons, round/floor to have no border
   return(
-      <Grid>
-        {[...Array(numcols)].map((x, i) =>
-          [...Array(numrows)].map((y, j) => 
-          <Hexagon key={i*numrows+j} 
+      <Grid onKeyDown={(e) => console.log(e.code)}>
+        {[...Array(numCols)].map((x, i) =>
+          [...Array(numRows)].map((y, j) => 
+          <Hexagon key={i*numRows+j} 
             hexsize={hexsize}
-            isactive={isActive[i*numrows+j]}
+            isactive={isActive[i*numRows+j]}
             // background={backgroundColor[i*numcols+j]}
-            style={{background:backgroundColor[i*numrows+j], 
-                    left:100+i*hexsize*3/4  + (hexsize/2) * i%2, 
+            style={{background:backgroundColor[i*numRows+j], 
+                    left:100+i*hexsize*0.75  + (hexsize/2) * i%2, 
                     top:150 + hexsize/2/Math.sqrt(3)*(i%2) + hexsize/Math.sqrt(3) * ((j))
                   }}
             onMouseDown={(e) => {
                   if(e.shiftKey){
-                      updateColor(i*numrows+j, 'black');
+                      updateColor(i*numRows+j, 'black');
                     }
                     else {
-                      updateColor(i*numrows+j, 'white');
+                      updateColor(i*numRows+j, 'white');
                     }
                   } }
             onMouseEnter={(e) => { 
                   if(isMouseDown){
                     if(e.shiftKey){
-                      updateColor(i*numrows+j, 'black');
+                      updateColor(i*numRows+j, 'black');
               
                     }
                     else {
-                      updateColor(i*numrows+j, 'white');
+                      updateColor(i*numRows+j, 'white');
                     }
                   }
             }}
 
           >
-            {((i*numrows+j))}<br/>{}{Math.floor((i*numrows+j)/2) % numrows < (numrows/2) ? '0' : '1'}
+            {/*((i*numrows+j))}<br/>{}{Math.floor((i*numrows+j)/2) % numrows < (numrows/2) ? '0' : '1'*/}
           </Hexagon>
   
       ))}
-      <ResetButton onClick={resetAll}/>
-      <StartButton isIterating={shouldIterate} onClick={() => setShouldIterate(!shouldIterate)}/>
-      <NextButton onClick={() => update()} isIterating={shouldIterate} />
-      <BoundaryButton onClick={(e) => updateBoundary(e.shiftKey ? false : true)} isIterating={shouldIterate} />
       </Grid>
   );
 }
@@ -344,7 +132,8 @@ const Hexagon = styled.div.attrs((props : {hexsize: number, top : number, left :
   clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
   // transform: rotateZ(0deg);
   user-select: none;
-transition: background 0.5s;
+  transition: background 1s, top 0.3s, left 0.3s;
+transition-delay: left 0.3s, top 0.3s;
   justify-content: center;
   align-content: center;
   vertical-align: middle;
@@ -366,92 +155,9 @@ const HexagonBorder = styled.div.attrs((props : {top : number, left : number, ba
 
 const Grid = styled.div`
   position: absolute;
-  background: gray;
+  background: rgb(80,80,80);
   width: 100%;
   height: 100%;
-`
-
-const ResetButton = styled.div`
-  width: 30px;
-  height: 30px;
-  border-radius: 15px;
-  background: rgb(150,0,0);
-  transition: background 0.1s;
-  position: absolute;
-  left: 70%;
-  top: 1%;
-  cursor: pointer;
-  &:hover{
-    background: rgb(225,0,0);
-  }  
-  &:active{
-    background: rgb(255,0,0);
-  }  
-
-
-`
-const StartButton = styled.div<{isIterating: boolean}>`
-  width: 30px;
-  height: 30px;
-  border-radius: 15px;
-  background: ${p => p.isIterating ? 'rgb(0,150,0)' : 'rgb(150,50,0)'};
-  transition: background 0.1s;
-  position: absolute;
-  left: 60%;
-  top: 1%;
-  cursor: pointer;
-
-  &:hover{
-    background: ${p => p.isIterating ? 'rgb(0,180,0)' : 'rgb(180,80,0)'};
-  }  
-
-  &:active{
-    background: ${p => p.isIterating ? 'rgb(0,220,0)' : 'rgb(220,110,0)'};
-  }  
-
-
-`
-const NextButton = styled.div<{isIterating: boolean}>`
-  width: 30px;
-  height: 30px;
-  border-radius: 15px;
-  background: ${p => p.isIterating ? 'rgb(150,0,150)' : 'rgb(150,0,150)'};
-  transition: background 0.1s;
-  position: absolute;
-  left: 65%;
-  top: 1%;
-  cursor: pointer;
-
-  &:hover{
-    background: ${p => p.isIterating ? 'rgb(190,0,190)' : 'rgb(190,0,190)'};
-  }  
-
-  &:active{
-    background: ${p => p.isIterating ? 'rgb(220,0,220)' : 'rgb(220,0,220)'};
-  }  
-
-
-`
-const BoundaryButton = styled.div<{isIterating: boolean}>`
-  width: 30px;
-  height: 30px;
-  border-radius: 15px;
-  background: ${p => p.isIterating ? 'rgb(0,0,150)' : 'rgb(0,0,150)'};
-  transition: background 0.1s;
-  position: absolute;
-  left: 75%;
-  top: 1%;
-  cursor: pointer;
-
-  &:hover{
-    background: ${p => p.isIterating ? 'rgb(0,0,180)' : 'rgb(0,0,180)'};
-  }  
-
-  &:active{
-    background: ${p => p.isIterating ? 'rgb(0,0,220)' : 'rgb(0,0,220)'};
-  }  
-
-
 `
 
 
